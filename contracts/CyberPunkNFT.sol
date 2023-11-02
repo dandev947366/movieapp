12c457afda4a3b4436d0732277c5bfb0d643dfcf
dandev947366 <dan.dev947366@gmail.com>
2023-11-02 14:24:27 +0200

    contract

---
+++ contracts/CyberPunkNFT.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
+import '@openzeppelin/contracts/access/Ownable.sol';
+contract CyberPunkNFT is ERC721, Ownable{
+
+    uint256 public mintPrice;
+    uint256 public totalSupply;
+    unit256 public maxSupply;
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
