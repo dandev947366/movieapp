294705b2502b4b300c9d882636ee0eaf5d58f9c8
dandev947366 <dan.dev947366@gmail.com>
2023-11-02 14:31:05 +0200

    contract

--- contracts/CyberPunkNFT.sol
+++ contracts/CyberPunkNFT.sol
@@ -8,16 +8,35 @@ contract CyberPunkNFT is ERC721, Ownable{
     uint256 public mintPrice;
     uint256 public totalSupply;
     unit256 public maxSupply;
+    uint256 public maxPerWallet;
+    bool public isPublicMintEnabled;
+    string internal baseTokenUri;
+    address payable public withdrawWallet;
+    mapping(address => uint256) public walletMints;
+    constructor() payable ERC721 ('CyberPunks', 'RP'){
+        mintPrice = 0.2 ether;
+        totalSupply = 0;
+        maxSupply = 1000;
+        maxPerWallet = 3;
+    }
+        //withdraw wallet
+        
+        function setIsPublicMintEnabled(bool isPublicMintEnabled_) external onlyOwner{
+        
+        isPublicMintEnabled = isPublicMintEnabled_;
+
+    }
+    
+    function setBaseTokenUri(string calldata passTokenUri_) external onlyOwner{
+    
+    setBaseTokenUri = setBaseTokenUri_;
+
+    }
+    
+    function tokenUri(uint256 tokenId_) public view oeverride returns (string memory){
     
     
-
-
-
-
-
-
-
-
+    }
 
 
 
