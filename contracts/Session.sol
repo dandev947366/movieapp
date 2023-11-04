84a1e4e6af04b89923d2fb14570eb17b551854ee
dandev947366 <dan.dev947366@gmail.com>
2023-11-04 18:19:58 +0200

    update

--- contracts/Session.sol
+++ contracts/Session.sol
@@ -11,7 +11,35 @@ contract Session {
     // Variable to hold Main Contract instance to call functions from Main
     Main MainContract;
 
-    // TODO: Variables
+    struct Item {
+        uint256 id,
+        string name,
+        address owner,
+        uint256 _tokenId,
+        uint256 initialPrice,
+        address previousBidder,
+        uint256 lastBid,
+        address lastBidder,
+        uint256 startTime,
+        uint256 endTime,
+        bool completed,
+        bool active,
+        uint256 auctionId
+    }
+    
+    event Action (
+        uint256 id,
+        string name,
+        address owner,
+        uint256 _tokenId,
+        uint256 initialPrice,
+        uint256 startTime,
+        uint256 endTime,
+        bool completed,
+        bool active,
+        uint256 auctionId
+        address indexed admin
+    );
 
     
     function Session(address _mainContract
