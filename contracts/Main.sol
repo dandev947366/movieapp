55f7b972b2d862e7c42af3e673894df2141c8863
dandev947366 <dan.dev947366@gmail.com>
2023-11-08 19:51:26 +0200

    update

--- contracts/Main.sol
+++ contracts/Main.sol
@@ -1,23 +1,43 @@
 pragma solidity ^0.4.17;
 
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./Session.sol";
-contract Main {
+contract Main is AccessControl {
 
     using Counters for Counters.Counter;
     Counters.Counter private _totalSessions;
+    Counters.Counter private _totalItems;
+    
     uint256 private capacity;
 
     struct Iparticipant{
-        address participant,
+        address participantWallet,
         bool isParticipate,
         uint256 NoOfSession,
+        uint256 NoOfBids,
         uint256 deviation
     }
-
+    
+    struct Item {
+        uint256 id,
+        string name,
+        string description,
+        address owner,
+        string imageURI,
+        uint256 initialPrice,
+        uint256 finalPrice
+    }
+    mapping(uint => Item) items;
+    mapping(uint => Session) sessions;
     mapping(uint => mapping(uint => adress) bidding;
     mapping(uint => Iparticipant[]) totalParticipants;
     mapping(uint => address) participantOf;
+    mapping(uint256 => bool) sessionExists;
+    mapping(uint256 => bool) itemExists;
+    
+    mapping(uint => mapping(uint => uint256[]) bids;
+    
     enum Status {
         OPEN
         RUNNING
@@ -29,8 +49,6 @@ contract Main {
         Status status,
         string message
     }
-    
-
 
     function Main() public {
         admin = msg.sender;
@@ -38,10 +56,47 @@ contract Main {
 
     
     // Add a Session Contract addrexss into Main Contract. Use to link Session with Main
-    function addSession(address session) public {
-        // TODO
+    function createItem(
+        string memory _name,
+        string memory _description,
+        string memory _imageURI,
+        uint256 _initialPrice
+    ) public onlyOwner returns (Item memory){
+        require(bytes(_name).length > 0, "Name cannot be empty");
+        require(bytes(_description).length > 0, "Description cannot be empty");
+        require(bytes(_imageURI).length > 0, "ImageURI cannot be empty");
+        require(_initialPrice > 0, "Initial price cannot be empty");
+        _totalItems.increment();
+        Item memory item;
+        item.id = _totalItems.current();
+        item.name = _name;
+        item.description = _description;
+        item.owner = admin;
+        item.imageURI = _imageURI;
+        item.initialPrice = __initialPrice;
+        itemExists[_totalItems.current()] = true;
+        items.push(item);
+        emit Action {"Create item successfully"};
+    }
+    
+    function addSession(
+        address _session
+        uint _itemId,
+    ) public onlyOwner returns(Session memory){
+        require(_itemId > 0, "Item ID cannot be empty");
+        require(bytes(_session).length > 0, "Session address cannot be empty");
         
+        address sessionContract = _sessionContract;
+        _totalSessions.increment();
         
+        Session memory session;
+        session.contractAddress = _session;
+        session.id = _totalSessions.current();
+        session.itemId = _itemId;
+        session.participant = Iparticipant[](10);
+        session.status = status.OPEN;
+        sessionExists[_totalSessions] = true;
+        sessions.push(session);
         
         emit Action {
             sessionId,
@@ -51,74 +106,92 @@ contract Main {
         }
         
     }
-    function updateSession(address session) public {
-        // TODO
+    
+    function updateSession(
+        uint256 _id,
+        Status status,
+        bool completed
+        bool deleted,
+
+    ) public onlyOnwer returns (bool){
+        require(sessionExists[_id], "Session not found");
+        require(bytes(status).length > 0, "Status cannot be empty");
         
-        emit Action {
-            sessionId,
-            noOfParticipants,
-            status,
-            "Update session successfully"
-        }
+        sessions[_id].id = id;
+        sessions[_id].status = status;
+        sessions[_id].completed = completed;
+        sessions[_id].deleted = ideleted;
+        emit Action {"Update session successfully"}
     }
-    function deleteSession(address session) public {
-        // TODO
+    function deleteSession(uint256 sessionId) public onlyOwner{
+        require(sessionExists[sessionId], "Session not Exsists");
         
-        
-         emit Action {
-            sessionId,
-            noOfParticipants,
-            status,
-            "Delete session successfully"
-        }
+        sessions[sessionId].deleted = true;
+        sessionExists[sessionId] = false;
+        emit Action {"Delete session successfully"}
     }
-    function getSession(address session) public {
-        // TODO
-        
-        
-         emit Action {
-            sessionId,
-            noOfParticipants,
-            status,
-            "Get session successfully"
-        }
+    function getSession(uint256 sessionId) public view returns(Session memory) {
+        return sessions[sessionId];
     }
-    function getAllSessions(address session) public {
-        // TODO
+    function getAllSessions() public view returns (Item[] memory){
+        uint256 available;
         
-        
-        emit Action {
-            sessionId,
-            noOfParticipants,
-            status,
-            "Get all sessions successfully"
+        for(uint256 i = 1; i <= _totalSessions.current(); i++){
+            if(!sessions[i].deleted] available++;
         }
+       
+        Session = new Session[](available);
+        
+        uint256 index; 
+        for(uint256 i = 1; i <= _totalMovies.current(); i++){
+           
+            if(!sessions[i].deleted) Session[index++] = sessions[i];
+        }
+        
     }
-    function getSessionsByParticipant(address session) public {
-        // TODO
+    function closeSession(uint256 _sessionId) public onlyOwner returns(bool){
+        require(sessionExists[sessionId], "Session not Exsists");
+        require(!sessions[sessionId].completed, "Session completed");
         
-        
-        
-        emit Action {
-            sessionId,
-            noOfParticipants,
-            status,
-            "Get sessions by participant successfully"
-        }
+        Session memory session;
+        session[_sessionId].completed = true;
+    
     }
-    function getSessionsByStatusession(address session) public {
-        // TODO
+    
+    function getItem(uint256 itemId) public view returns(Item memory){
+        return items[itemId];
+    }
+    
+    function updateItem(
+        uint256 _id,
+        string memory _name,
+        string memory _description,
+        string memory _imageURI,
+        uint256 _initialPrice,
+    ) public onlyOwner returns(bool){
+        require(_id > 0, "Id cannot be empty");
+        require(bytes(_name).length > 0, "Name cannot be empty");
+        require(bytes(_description).length > 0, "Description cannot be empty");
+        require(bytes(_imageURI).length > 0, "ImageURI cannot be empty");
+        require(_initialPrice > 0, "Price cannot be empty"));
+        items[id].id = _id;
+        items[id].name = _name;
+        items[id].description = _description;
+        items[id].imageURI = _imageURIid;
+        items[id] = _id;
+        items[id].initialPrice = _initialPrice;
+        emit Action {"Update Item successfully"};
         
+    }
+    
+    function deleteItem(uint256 _id) public onlyOwner returns(bool){
+        require(itemExists[_id], "Item not found");
         
+        items[_id].deleted = true;
+        itemExists[_id] = false;
         
-        
-        
-        emit Action {
-            sessionId,
-            noOfParticipants,
-            status,
-            "Get sessions by status successfully"
-        }
+        emit Action("Item deleted successfully");
+    
     }
 
 
--- contracts/Session.sol
+++ contracts/Session.sol
@@ -16,10 +16,17 @@ contract Session {
 
     using Counters for Counters.Counter;
     Counters.Counter private _totalItem;
+    Counters.Counter private _NoOfParticipant;
     
-    
-    
-    
+    struct Session{
+        address contractAddress,
+        uint256 id,
+        uint256 itemId,
+        Iparticipant[] participantList,
+        Status status,
+        bool deleted,
+        bool completed
+    }
     
     function Session(address _mainContract
         // Other arguments
@@ -33,7 +40,36 @@ contract Session {
         // Call Main Contract function to link current contract.
         MainContract.addSession(address(this));
     }
+    
+    function joinSession(unint256 _sessionId) public returns(bool){
+        require(sessionExists[sessionId], "Session not Exists");
+        require(!sessions[_sessionId].completed, "Session completed");
+        _NoOfParticipant++;
+        Iparticipant memory participant;
+        participant[msg.sender].participantWallet = msg.sender;
+        participant[msg.sender].isParticipate = true;
+        participant[msg.sender].NoOfSession++;
+        
+        emit Action ("Join session successfully");
+    }
 
-    // TODO: Functions
+    function startBidding(unint256 _sessionId, uint256 _itemId, uint256 _amount) public returns(bool){
+        require(itemExists[_id], "Item not found");
+        require(sessionExists[sessionId], "Session not Exists");
+        require(msg.value >= _amount, "Insufficient balance");
+        require(bids[_itemId][_sessionId] <= 10, "Maximum participant reached");
+        
+        bids[_itemId][_sessionId].push(_amount)
+        
+        Iparticipant memory participant;
+        participant[msg.sender].NoOfBids++;
+        
+        Session memory session;
+        session[_sessionId].participantList.push(msg.sender);
+        session[_sessionId].status = RUNNING;
+        
+        emit Action ("Bid item successfully");
+    }
+    
     
 }
